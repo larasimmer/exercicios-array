08d59c124d099324fdcf3e7c71b81212b314623b
Lara Simmer <larafsimmer@gmail.com>
2021-12-30 19:07:17 -0300

    Exercício de concatenação de arrays

---
+++ juntando-salas.js
@@ -0,0 +1,7 @@
+//Desafio: juntar duas listas existentes
+
+const alunosPython = ["Melissa", "Helena", "Rodrigo"];
+const alunosJavascript = ["Ju", "Leo", "Raquel"];
+
+const salasJuntas = alunosPython.concat(alunosJavascript);
+console.log(salasJuntas);
