2ec665919af1df9ea29a9ad952cbb8d1c1c52940
Lara Simmer <larafsimmer@gmail.com>
2021-12-30 15:52:55 -0300

    Adicionando elementos em um array

---
+++ adicionando-elementos-array.js
@@ -0,0 +1,12 @@
+//Desafio - Cálculo de médias
+
+//Adicionar um elemento no array
+
+const notas = [10, 6, 8];
+notas.push(7);
+
+console.log(notas);
+
+let mediaNotas = (notas[0] + notas[1] + notas[2] + notas[3])/notas.length;
+
+console.log(mediaNotas);
